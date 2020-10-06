175f19675b3f63765dd1b54f1fd01f442e1a1b2d
MullaN <admullan@gmail.com>
2020-10-05 17:20:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, holiday){
+    let array = [];
+    for (let i = 0; i < names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+    }
+    return array;
+}
+
+function countDown(start){
+    while (start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
