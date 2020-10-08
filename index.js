51aa2842d3b2c1a7de9178891b61a6d880aa7eb8
jonelle-noelani <jonelle.noelani@gmail.com>
2020-10-08 10:56:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let fullMessages = [ ]
+
+function writeCards(array, event){
+    for (let i = 0; i < array.length; i++){
+        fullMessages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return fullMessages;
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num); 
+        num -= 1;
+    }
+}
